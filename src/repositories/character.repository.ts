9e2f2074362fa3796b635bb51a2963ad856545a2
Kapa <kapa123@email.com>
2026-04-22 21:24:04 +0200

    Implementcja wzorca Repository dla postaci

---
+++ src/repositories/character.repository.ts
@@ -0,0 +1,26 @@
+import { Character, ICharacter } from '../models/character.model';
+
+class CharacterRepository {
+
+    async findAll(): Promise<ICharacter[]> {
+        return await Character.find().populate('bestFriend');
+    }
+
+
+    async findById(id: string): Promise<ICharacter | null> {
+        return await Character.findById(id).populate('bestFriend');
+    }
+
+
+    async create(characterData: Partial<ICharacter>): Promise<ICharacter> {
+        const character = new Character(characterData);
+        return await character.save();
+    }
+
+
+    async delete(id: string): Promise<ICharacter | null> {
+        return await Character.findByIdAndDelete(id);
+    }
+}
+
+export default new CharacterRepository();
